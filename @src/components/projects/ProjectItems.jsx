65debb13a1888b72982edcdb172099bb067d608e
Andrew <andrew@frisson.info>
2020-06-05 18:58:17 +0200

    Updated Projects

--- @src/components/projects/ProjectItems.jsx
+++ @src/components/projects/ProjectItems.jsx
@@ -1,53 +1,50 @@
 
 import { React } from '../../chunk-e.js';
-import { VisionLogo, FloatingColorsLogo, GalleryLogo, TiarLogo, PhotographyLogo, KeyboardTrainerLogo, APianoLogo } from './ProjectLogotypes.jsx';
+import { VisionLogo, FloatingColorsLogo, GalleryLogo, TiarLogo, PhotographyLogo, ATeacher, APianoLogo } from './ProjectLogotypes.jsx';
 
 
 export const ProjectItems = [
 
     {
         title: 'Tiar',
-        description: 'Training in any reality',
-        logo: <TiarLogo/>,
-        solution: true
+        description: 'Coming soon...',
+        logo: <TiarLogo/>
     },
     {
         title: 'A.Piano',
         description: 'A beautiful piano (HQ, HiRez)',
         logo: <APianoLogo/>,
-        url: 'https://andrey-brill.github.io/a-piano',
-        solution: true
+        url: 'https://andrey-brill.github.io/a-piano'
+    },
+    {
+        title: 'A.Teacher',
+        description: 'Listen. Type. Learn.',
+        logo: <ATeacher/>,
+        url: 'https://andrey-brill.github.io/a-teacher'
     },
     {
         title: 'Floating colors',
         description: 'Adding SMIL animation to SVG',
         logo: <FloatingColorsLogo/>,
-        url: 'https://github.com/andrey-brill/a-floating-colors',
-        solution: true
-    },
-    {
-        title: 'Keyboard Trainer',
-        description: 'Typing with closed eyes',
-        logo: <KeyboardTrainerLogo/>,
-        solution: true
-    },
-
-    {
-        title: 'Photography',
-        description: "Journey through my memories",
-        logo: <PhotographyLogo/>,
-        solution: false
-    },
-    {
-        title: 'Vision',
-        description: 'World through me',
-        logo: <VisionLogo/>,
-        solution: false
-    },
-    {
-        title: 'Gallery',
-        description: 'Gallery of inspirational art',
-        logo: <GalleryLogo/>,
-        solution: false
+        url: 'https://github.com/andrey-brill/a-floating-colors'
     }
+
+    // {
+    //     title: 'Photography',
+    //     description: 'Coming soon...', // "Journey through my memories",
+    //     logo: <PhotographyLogo/>,
+    //     solution: false
+    // },
+    // {
+    //     title: 'Vision',
+    //     description: 'Coming soon...', // 'World through me',
+    //     logo: <VisionLogo/>,
+    //     solution: false
+    // },
+    // {
+    //     title: 'Gallery',
+    //     description: 'Gallery of inspirational art',
+    //     logo: <GalleryLogo/>,
+    //     solution: false
+    // }
 ];
