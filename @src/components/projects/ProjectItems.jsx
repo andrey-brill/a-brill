09e7728ec80f421ddba221f6272531874ae8050f
Andrew <andrew@frisson.info>
2020-12-05 14:39:47 +0100

    Reordered my projects

--- @src/components/projects/ProjectItems.jsx
+++ @src/components/projects/ProjectItems.jsx
@@ -3,31 +3,10 @@
 export const ProjectItems = [
 
     {
-        key: 'a-brill',
-        title: 'ABrill',
-        description: 'Own brand design.',
-        url: 'https://a-brill.com'
-    },
-
-    {
-        key: 'a-gallery',
-        title: 'AGallery',
-        description: 'Artworks with an accent on the artists\' visions.',
-        url: 'https://www.instagram.com/abrill_gallery'
-    },
-
-    {
-        key: 'a-photography',
-        title: 'APhotography',
-        description: 'Digital journey through my memories.',
-        url: 'https://www.instagram.com/abrill_photography'
-    },
-
-    {
-        key: 'a-vision',
-        title: 'AVision',
-        description: 'Art photography.',
-        url: 'https://www.instagram.com/abrill_vision'
+        key: 'a-chess',
+        title: 'AChess',
+        description: 'Android app with modern chess puzzles.',
+        url: 'https://play.google.com/store/apps/details?id=com.abrill.achess'
     },
 
     {
@@ -38,10 +17,10 @@ export const ProjectItems = [
     },
 
     {
-        key: 'a-chess',
-        title: 'AChess',
-        description: 'Android app with modern chess puzzles.',
-        url: 'https://play.google.com/store/apps/details?id=com.abrill.achess'
+        key: 'a-vision',
+        title: 'AVision',
+        description: 'Art photography.',
+        url: 'https://www.instagram.com/abrill_vision'
     },
 
     {
@@ -62,6 +41,28 @@ export const ProjectItems = [
         key: 'a-chrome-extensions',
         title: 'AExtensions',
         description: 'Useful extensions for the Chrome browser.',
-        url: 'https://chrome.google.com/webstore/search/ABrill?hl=en&_category=extensions'
-    }
+        url: 'https://chrome.google.com/webstore/search/Brill?hl=en&_category=extensions'
+    },
+
+    {
+        key: 'a-photography',
+        title: 'APhotography',
+        description: 'Digital journey through my memories.',
+        url: 'https://www.instagram.com/abrill_photography'
+    },
+
+    {
+        key: 'a-gallery',
+        title: 'AGallery',
+        description: 'Artworks with an accent on the artists\' visions.',
+        url: 'https://www.instagram.com/abrill_gallery'
+    },
+
+    {
+        key: 'a-brill',
+        title: 'ABrill',
+        description: 'Own brand design.',
+        url: 'https://a-brill.com'
+    },
+
 ];
